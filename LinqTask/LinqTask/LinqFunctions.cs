519ba330a6cac48af3fd1c74a772cc70a0415bba
chrisballeto <chrisballeto@gmail.com>
2017-01-06 12:21:55 -0600

    periodic save

---
+++ LinqTask/LinqTask/LinqFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTask
+{
+    class LinqFunctions
+    {
+
+
+
+        public List<string> GetUnDuplicated(List<string> list)
+        {
+            List<string> noDupsList = new List<string>();
+            noDupsList = list.Distinct().ToList();
+            return noDupsList;
+        }
+
+        public int GetAverageScore(List<string> list)
+        {
+            int average = 0;
+            return average;
+        }
+
+        public string GetNumberOfEachLetter()
+        {
+            string nameNumbered;
+
+
+            return nameNumbered;
+        }
+
+
+        
+    }
+}
--- LinqTask/LinqTask/Program.cs
+++ LinqTask/LinqTask/Program.cs
@@ -10,6 +10,31 @@ namespace LinqTask
     {
         static void Main(string[] args)
         {
+            List<string> dupsList = new List<string>() { "cat", "dog", "cat", "bird" };
+            List<string> grades1 = new List<string>() {"90,76,89,55,22" ,"98,54,97,72,66" ,"87,25,36,86,47" };
+            //List<int> grades2 = new List<int>() { "98, 54, 97, 72, 66" };
+            //List<int> grades3 = new List<int>() { "87, 25, 36, 86, 47" };
+
+            LinqFunctions runFunctions = new LinqFunctions();
+            List<string> newList = new List<string>();
+            newList = runFunctions.GetUnDuplicated(dupsList);
+
+            int classAverage = runFunctions.GetAverageScore(grades1);
+            Console.WriteLine(classAverage);
+
+
+
+
+
+
+
+            Console.ReadLine();
+        
+
+            
         }
+
     }
 }
+
+
