020d1fda8243b00f6792884090d0f74d1a7f5f96
chrisballeto <chrisballeto@gmail.com>
2017-01-09 06:57:30 -0600

    assignment turn in

--- LinqTask/LinqTask/LinqFunctions.cs
+++ LinqTask/LinqTask/LinqFunctions.cs
@@ -6,33 +6,46 @@ using System.Threading.Tasks;
 
 namespace LinqTask
 {
-    class LinqFunctions
+    class LinqFunctions 
     {
 
 
-
+        //Function #1
         public List<string> GetUnDuplicated(List<string> list)
         {
             List<string> noDupsList = new List<string>();
             noDupsList = list.Distinct().ToList();
             return noDupsList;
         }
-
-        public int GetAverageScore(List<string> list)
+        //Function #2
+        public double GetAverageScore(List<string> list)
         {
-            int average = 0;
-            return average;
+            List<double> scoresAsDub = new List<double>();
+            foreach(string s in list)
+            {          
+                List<int> scores = (s.Split(',').Select(nums => int.Parse(nums)).ToList());
+                scores = scores.Where(x => x != scores.Min()).ToList();
+                var scoreAverage = scores.Average();
+                scoresAsDub.Add(scoreAverage);
+            }
+            return Math.Round(scoresAsDub.Average(), 2);          
+        }
+        //Function #3
+        public string GetNumberOfEachLetter(string input)
+        {
+            var chars =  input.ToUpper().ToCharArray();
+            var letters = (from c in chars
+                           orderby c
+                           select c).ToList();
+            var pair = letters.GroupBy(c => c);
+            foreach(var num in pair)
+            {
+                Console.Write("{0}{1}", num.Key, num.Count());
+            }
+            return null;
         }
 
-        public string GetNumberOfEachLetter()
-        {
-            string nameNumbered;
 
 
-            return nameNumbered;
-        }
-
-
-        
     }
 }
--- LinqTask/LinqTask/Program.cs
+++ LinqTask/LinqTask/Program.cs
@@ -10,28 +10,22 @@ namespace LinqTask
     {
         static void Main(string[] args)
         {
-            List<string> dupsList = new List<string>() { "cat", "dog", "cat", "bird" };
-            List<string> grades1 = new List<string>() {"90,76,89,55,22" ,"98,54,97,72,66" ,"87,25,36,86,47" };
-            //List<int> grades2 = new List<int>() { "98, 54, 97, 72, 66" };
-            //List<int> grades3 = new List<int>() { "87, 25, 36, 86, 47" };
+            List<string> dupsList = new List<string>() { "cat", "dog", "cat", "bird", "bird", "falcon", "falcon", "kitten" };
+            List<string> grades = new List<string>() {"90,76,89,55,22" ,"98,54,97,72,66" ,"87,25,36,86,47" };
 
+            //Function #1
             LinqFunctions runFunctions = new LinqFunctions();
-            List<string> newList = new List<string>();
-            newList = runFunctions.GetUnDuplicated(dupsList);
+            List<string> newList = runFunctions.GetUnDuplicated(dupsList);           
+            newList.ForEach(Console.WriteLine);
 
-            int classAverage = runFunctions.GetAverageScore(grades1);
+            //Function #2
+            double classAverage = runFunctions.GetAverageScore(grades);
             Console.WriteLine(classAverage);
 
+            //Function #3
+            Console.WriteLine(runFunctions.GetNumberOfEachLetter("Balleto"));
 
-
-
-
-
-
-            Console.ReadLine();
-        
-
-            
+            Console.ReadLine();        
         }
 
     }
